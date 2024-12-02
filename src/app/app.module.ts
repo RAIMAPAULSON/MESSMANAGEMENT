8bb191a162ecfeb07d99f264a2d01446494fc2e3
RAIMAPAULSON <raimapaulson11@gmail.com>
2024-12-02 17:21:26 +0530

    project completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpenseManagementComponent } from './components/expense-management/expense-management.component';
+import { MealManagementComponent } from './components/meal-management/meal-management.component';
+import { ReportingComponent } from './components/reporting/reporting.component';
+import { UserManagementComponent } from './components/user-management/user-management.component';
+import { FormsModule } from '@angular/forms'; // Add this line
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpenseManagementComponent,
+    MealManagementComponent,
+    ReportingComponent,
+    UserManagementComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  // User APIs
+  getUsers(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/users`);
+  }
+  addUser(user: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/users`, user);
+  }
+  updateUser(id: number, user: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/users/${id}`, user);
+  }
+  deleteUser(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/users/${id}`);
+  }
+
+  // Fetch all meals
+  getMeals(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/meals`);
+  }
+
+  // Add a new meal
+  addMeal(meal: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/meals`, meal);
+  }
+
+  // Update an existing meal
+  updateMeal(id: number, meal: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/meals/${id}`, meal);
+  }
+
+  // Delete a meal
+  deleteMeal(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/meals/${id}`);
+  }
+
+  // Get Expenses
+  getExpenses(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/expenses`);
+  }  
+  // Add Expense
+  addExpense(expense: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/expenses`, expense);
+  }
+
+  // Delete Expense
+  deleteExpense(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/expenses/${id}`);
+  }
+
+}
